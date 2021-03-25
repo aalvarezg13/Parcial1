ebe3d00de20ec9c833c77d1a41cb774aa1f1c87e
leysa <leysa@DESKTOP-H2S0AUO>
2021-03-25 10:45:26 -0500

    version 1

---
+++ src/main/java/parcial/parcial1/Producto.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial.parcial1;
+
+/**
+ *
+ * @author leysa
+ */
+public class Producto {
+    
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, int cantidad, double precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+    
+}
